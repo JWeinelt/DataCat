6944a8534d1a74186860e02c348d8378c19f8f8d
Julian Weinelt <julian.weinelt@outlook.de>
2026-03-13 09:14:36 +0100

    docs: optimize landing page

--- docs/src/components/HomepageFeatures/index.js
+++ docs/src/components/HomepageFeatures/index.js
@@ -4,32 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Everything in your database',
+    Svg: require('@site/static/img/undraw_database-tables_yft5.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Manage everything of your database(s) in just one application.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Secure collaboration',
+    Svg: require('@site/static/img/undraw_data-processing_ohfw.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Process your data in just one platform and share it with your team.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Schedule SQL jobs',
+    Svg: require('@site/static/img/undraw_schedule_ry1w.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Schedule SQL Scripts as jobs, no matter, what type of database engine you are using.
       </>
     ),
   },
