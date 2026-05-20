1ec09836f1bed7890321c31d51fc79e1a0848373
Julian Weinelt <julian.weinelt@outlook.de>
2026-05-20 11:27:59 +0200

    api: improve argument value definitions

--- dbx/src/main/java/de/julianweinelt/datacat/dbx/startup/ArgumentParser.java
+++
@@ -1,5 +0,0 @@
-package de.julianweinelt.datacat.dbx.startup;
-
-public interface ArgumentParser {
-    Argument parse(StartArguments args, int index);
-}
--- dbx/src/main/java/de/julianweinelt/datacat/dbx/startup/ArgumentValue.java
+++ dbx/src/main/java/de/julianweinelt/datacat/dbx/startup/ArgumentValue.java
@@ -1,17 +1,35 @@
 package de.julianweinelt.datacat.dbx.startup;
 
+import lombok.Getter;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.regex.Pattern;
 
+@Getter
 public class ArgumentValue extends Argument {
     private String value;
     private Pattern pattern;
     private boolean patternRequired;
     private String valueDescription;
 
-    public ArgumentValue(String baseName, ArgumentType type, String value) {
-        super(baseName, type);
+    public ArgumentValue(String baseName, String value) {
+        super(baseName, ArgumentType.ARGUMENT_PROPERTY);
+        this.value = value;
+    }
+
+    public ArgumentValue pattern(Pattern pattern) {
+        this.pattern = pattern;
+        return this;
+    }
+
+    public ArgumentValue patternRequired(boolean required) {
+        this.patternRequired = required;
+        return this;
+    }
+
+    public ArgumentValue description(String description) {
+        this.valueDescription = description;
+        return this;
     }
 
     @Override
